d0d2240a1eea99fe496885fbe0f830a2be6cb004
Duke Ko <dukhyunko93@gmail.com>
2020-05-21 15:50:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,11 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 
-function addElementToEndOfArray(array,element){
+function addElementToEndOfArray(array,element) {
   return[...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
+  return array
 }
